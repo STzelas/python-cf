cf2627d472a5b72e004a8514867d2b8c9889a8c9
Sot. Tzelatidis <stzelati@gmail.com>
2025-06-21 14:41:51 +0300

    feat: literals added with type prints

---
+++ chapter01/02_my_literals.py
@@ -0,0 +1,34 @@
+# ints
+num1 = 10 # Decimal format
+num2 = 0b1010 # Binary format
+num3 = 0x000A # Hexadecimal format
+
+# str
+str1 = 'CF'
+str2 = "CF"
+str3 = 'Cod\
+      ing'
+
+str4 = '''Hello
+Coding
+Factory'''
+
+str5 = """Hello
+Again
+CF"""
+
+# bool
+my_bool = True
+
+print("Types")
+
+# παίρνει ενα object και επιστρέφει τον τύπο
+print(type(num1))
+print(type(num2))
+print(type(num3))
+print(type(str1))
+print(type(str2))
+print(type(str3))
+print(type(str4))
+print(type(str5))
+print(type(my_bool))
