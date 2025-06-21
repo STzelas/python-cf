9df76793ffb61b54e8d0e1e128ade726ee685d33
Sot. Tzelatidis <stzelati@gmail.com>
2025-06-21 15:37:15 +0300

    feat: input demo added

---
+++ chapter01/05_input_demo.py
@@ -0,0 +1,8 @@
+name = input("Please enter your name: ") # Επιστρέφει πάντα String
+
+print("Hello", name)
+
+# year_of_birth = input("Please insert the year of your birth: ")
+# year_of_birth = int(year_of_birth)
+
+print("You are", 2025 - int(input("Please insert the year of your birth: ")), "years old")
