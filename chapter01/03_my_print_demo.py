131ef43d51bd3e3f8cb23bd937669e2316527c76
Sot. Tzelatidis <stzelati@gmail.com>
2025-06-21 14:52:38 +0300

    feat: printing with overloading print added

---
+++ chapter01/03_my_print_demo.py
@@ -0,0 +1,16 @@
+my_num = 10
+my_float = 3.14
+my_str = "Hello Cf"
+
+print("Printing comma separated values")
+print(my_num, my_float, my_str)
+
+print("------------")
+
+# Mporo na allaksw ton separator του print()
+print(my_num, my_float, my_str, sep="\t")
+
+
+print("------------")
+
+print(my_num, my_float, my_str, sep="\t", end="***")
