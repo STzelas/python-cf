c4b1abfbc02cf8971fbb3cb8ee53bd35effb66f4
Sot. Tzelatidis <stzelati@gmail.com>
2025-06-24 21:04:12 +0300

    feat: floats example added

---
+++ chapter01/10_floats_demo.py
@@ -0,0 +1,9 @@
+PI = 3.1415926535
+
+print(f"Value of PI: {PI}")
+print(f"Type of PI: {type(PI)}")
+
+small_num = 2.5e-3 # 2.5 * 10^(-3) or 0.025
+
+print(f"Value of Small num: {small_num}")
+print(f"Type of small num: {type(small_num)}")
