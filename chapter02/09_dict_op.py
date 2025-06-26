0e6467c1104a3aa363b8471c52a4cdf39123bec7
Sot. Tzelatidis <stzelati@gmail.com>
2025-06-26 19:14:08 +0300

    feat: new examples added

--- chapter02/09_dict_op.py
+++ chapter02/09_dict_op.py
@@ -20,7 +20,7 @@ print(f"Initial dict: {products}, Type: {type(products)}")
 # αν υπάρχει ξανά βάλτο με value + 1
 products[3] = "orange"
 print(f"Dict after insert: {products}")
-
+print("---------------------")
 # read an element (access)
 # product_1 = products[1]
 # print(product_1)
@@ -29,3 +29,41 @@ print(f"Dict after insert: {products}")
 # print(product_10)
 product_10 = products.get(10, "Out of stock")
 print(product_10)
+print("---------------------")
+# delete
+del products[1] # κάνει delete το key:value pair
+print(f"After deleting key 1 { products }")
+
+removed_item = products.pop(10, "Item not found") # default value  στην pop σε περίπτωση που δεν υπάρχει το key
+print(f"removed item: {removed_item}")
+print(f"After removal: {products}")
+
+print("---------------------")
+# remove the 'last' inserted item with popitem()
+key, value = products.popitem() # επιστρέφει μια πλοιάδα όπου το 1ο στοιχείο είναι int και το 2ο str
+print(f"Key: {key}, Value: {value}")
+print(products)
+
+print("---------------------")
+key_to_check = 2
+
+if(key_to_check in products):
+  print(f"Key {key} exists")
+else:
+  print(f"Key: {key_to_check} does not exist")
+
+print("---------------------")
+# iterate
+for key in products:  # με in μέσα σε λεξικό κοιτάμε τα keys
+  print(key, ":", products[key]) # αν θέλω το και το value ετσι
+print("---------------------")
+for key in products.keys(): # το ίδιο, καλύτερα έτσι για readability
+  print(key, ":", products[key])
+
+print("---------------------")
+for value in products.values(): # Εκτυπώνω μόνο τα values
+  print(value)
+
+print("---------------------")
+for key, value in products.items(): # Εκτυπώνω και τα keys και τα values
+  print(key, ":", value)
