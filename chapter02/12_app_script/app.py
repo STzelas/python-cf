e6a034960969e43a074d97983af00482c01f7387
Sot. Tzelatidis <stzelati@gmail.com>
2025-06-26 23:50:28 +0300

    feat: more imports and num1, num2 added in my_add

--- chapter02/12_app_script/app.py
+++ chapter02/12_app_script/app.py
@@ -3,10 +3,10 @@
 # και όχι όλο το module που πχ αν είχε
 # παρα πολλές methods που δεν τις ήθελα
 # θα ήταν ανούσιο και inefficient
-from my_calculations import my_add_function 
+from my_calculations import my_add_function as my_add, num1
 
 num2 = 10
 
 # res = my_calculation.my_add_function(200, 300)
-res = my_add_function(100, 200)
+res = my_add(num1, num2)
 print(res) # result 300
