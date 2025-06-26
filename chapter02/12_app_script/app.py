d4239c4011a7d84958f3dc194e87d26ffac755a3
Sot. Tzelatidis <stzelati@gmail.com>
2025-06-26 23:44:59 +0300

    feat: app example with imports added

---
+++ chapter02/12_app_script/app.py
@@ -0,0 +1,8 @@
+# import my_calculations
+from my_calculations import my_add_function # import μόνο την function που ήθελα
+
+num2 = 10
+
+# res = my_calculation.my_add_function(200, 300)
+res = my_add_function(100, 200)
+print(res)
