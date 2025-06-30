93f8b1a90257f11eba55b925ca7f9be0052bdf35
Sot. Tzelatidis <stzelati@gmail.com>
2025-06-30 21:47:23 +0300

    feat: immutable demo added

---
+++ chapter03/01_immutable_demo.py
@@ -0,0 +1,10 @@
+# Immutability
+# Επιστρέφει το id του variable με την built-in id()
+def print_id(variable_name, variable):
+  print(f"id({variable_name}) = ({id(variable)})")
+
+def main():
+  pass
+
+if __name__ == "__main__":
+  main()
