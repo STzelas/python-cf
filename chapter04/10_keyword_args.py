56bdcb5eba9600f67ac8121aa9a08813dbaff712
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-04 22:32:14 +0300

    feat: docs added

--- chapter04/10_keyword_args.py
+++ chapter04/10_keyword_args.py
@@ -4,9 +4,20 @@ from typing import List, Tuple, Dict, Optional
 
 # args με 2 αστεριακια παραπέμπουν σε λεξικά dict
 # kwargs -> κλειδί - τιμή
-def get_results(products, **kwargs):
+def get_results(products: List[Tuple[str, int]], **kwargs: Dict[str, str | int]) -> List[Tuple[str, int]]:
+  """
+  Filters a list of products based on given keyword arguments.
+  Each keyword arguments corresponds to a product attribute.
+
+  Params:
+    products (List[Tuple[str, int]]): A list of tuples where each tuple contains a brand and a price.
+    **kwargs (Dict[str, str | int]): Arbitrary keyword arguments for filtering.
+
+  Returns:
+    results (List[Tuple[str, int]]): List of products
+  """
   results = [
-    (brand, price) for brand, price in products if kwargs.get('brand') == brand and kwargs.get('price') >= price
+    (brand, price) for brand, price in products if kwargs.get('brand') == brand and kwargs.get('price') == price
   ]
   return results
 
