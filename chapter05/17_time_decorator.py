c9f437e1a2e0209a105c8a863249784a783a344f
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-08 22:22:09 +0300

    feat: decorators introduction example added

---
+++ chapter05/17_time_decorator.py
@@ -0,0 +1,36 @@
+# Decorator tutorial, best example
+import time
+
+def timer_decorator(func):
+  """
+  Decorator to measure the execution time of a function.
+
+  Params:
+    func (function): The function to be decorated.
+
+  Returns:
+    function: The decorated function with added timing functionality.
+  """
+  def innter_function(*args, **kwargs):
+    start_time = time.time()
+    result = func(*args, **kwargs) # execution
+    end_time = time.time()
+    print(f"{func.__name__} took {end_time - start_time} seconds to run.")
+    return result
+  return innter_function
+
+def sum_function(n):
+  return sum(range(n))
+
+my_sum_func = timer_decorator(sum_function)
+print(my_sum_func(1_000_000))
+
+# Decorators
+@timer_decorator # decorate με επιπλέον functionality αυτής της function
+def average_function(n):
+  if n == 0:
+    return 0
+  total_sum = sum(range(n))
+  return total_sum / n
+
+print(average_function(100))
