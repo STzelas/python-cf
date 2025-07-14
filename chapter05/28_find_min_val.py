7b97d79b5615e2785c51a65f70e9d13549dfdf4c
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-14 23:28:28 +0300

    refactor: typos

--- chapter05/28_find_min_val.py
+++ chapter05/28_find_min_val.py
@@ -12,7 +12,7 @@ def main():
   print(f"Student with the lowest alphabetical order: {student_with_min_alphabet_name}")
 
   # Student with min grade
-  student_with_min_grade = min(students, key=students.get) # δεν καλώ την get αλλα του λέω θα εφαρμώσεις ως get την fucntion get πάνω στο students και τα αποτελεσματα θα τα filtrareis και θα φερεις το min
+  student_with_min_grade = min(students, key=students.get) # δεν καλώ την get αλλα του λέω θα εφαρμόσεις ως get την function get πάνω στο students και τα αποτελεσματα θα τα filtrareis και θα φερεις το min
   print(f"Student with the lowest grade: {student_with_min_grade}")
 
   # Student with the shortest name by length
